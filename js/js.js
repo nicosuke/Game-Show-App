8e8dcb5dfc1ac90d04d86f23e195de1bc0d6880b
Nico Apablaza <73095954+nicosuke@users.noreply.github.com>
2021-04-10 18:17:26 +1200

    yo

---
+++ js/js.js
@@ -0,0 +1,13 @@
+const keyboard = document.getElementById("querty");
+const phase = document.getElementById("phase");
+const boton = document.getElementsByClassName("btn__reset");
+let missed = 0;
+
+const phrases = [
+    "hello world",
+    "I like fish",
+    "cara de papa",
+    "my parrot is cute",
+    "alex will bite you",
+    "welcome to my house"
+];
